7a45bdd64682b243b1d3eec02e6c5816551e08b5
Brock T <brockthigpen@yahoo.com>
2020-03-21 14:01:22 -0400

    added different gatsby starter

---
+++ gatsby-browser.js
@@ -0,0 +1,13 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+// Global style files for MDBReact
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+import "./src/styles/scss/mdb.scss"
