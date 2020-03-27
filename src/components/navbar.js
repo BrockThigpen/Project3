2ab4263ef34a32b329287bc37d681a06c0c43e77
Brock T <brockthigpen@yahoo.com>
2020-03-26 20:08:52 -0400

    solved conflicts

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
-<<<<<<< HEAD
-import logo from "../images/appname.png"
-=======
 import AuthBtn from './authBtn.js'
->>>>>>> 74174acef741c5ce96d0c8d0195f4b7cd7e8453d
 import { Link } from "gatsby"
 import {
   MDBNavbar,
@@ -30,13 +26,8 @@ class Navbar extends Component {
       <MDBNavbar color="primary-color" dark expand="md">
         <MDBContainer>
           <MDBNavbarBrand>
-<<<<<<< HEAD
-            <Link to="/">
-              <img src={logo} alt="website logo" />
-=======
             <Link to="/page-2">
               <strong className="white-text">{this.props.siteTitle}</strong>
->>>>>>> 74174acef741c5ce96d0c8d0195f4b7cd7e8453d
             </Link>
           </MDBNavbarBrand>
           <MDBNavbarToggler onClick={this.toggleCollapse} />
