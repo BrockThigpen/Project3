744ffa260308f8b3f2f81924f8e912fdf0ada987
Josh Craig <jcraig451@yahoo.com>
2020-03-29 17:05:52 -0400

    deleted old files and adding usability to new app

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import logo from "../images/appname_big.png"
+import { Link } from 'react-router-dom'
 
 const Home = (props) => {
 	return (
-		<div className='Home'>
-			<h1>Strategic Machines</h1>
-			<h2>Secure Messaging</h2>
-			<h3>JSON Web Token Demo</h3>
+		<div className="homeContainer text-center">
+			<h1><strong className="text-white">Welcome to</strong></h1>
+			<br></br>
+			<img src={logo} alt="website logo" width="80%" height="auto" />
+			<hr className="my-4" style={{ border: "3px solid red", margin: "15%" }} />
+			<br></br>
+			<h4><strong className="text-white">Please sign in below to continue...</strong></h4>
+			<br></br>
+			<br></br>
+				<button className="selBtn">
+				<Link to="/login">Log In</Link>
+					</button>
+					<br>
+					</br>
+					<br>
+					</br>
+				<button className="selBtn">
+				<Link to="/signup">Sign Up</Link>
+				</button>
 		</div>
 	)
 }
