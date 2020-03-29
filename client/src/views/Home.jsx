1e37d251dda5259a46aebb7755f4600e10dd0231
Josh Craig <jcraig451@yahoo.com>
2020-03-29 18:40:04 -0400

    updated home page and linked login and create account

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import logo from "../images/appname_big.png"
+		import { Link } from 'react-router-dom'
+
 
 const Home = (props) => {
 	return (
-		<div className='Home'>
-			<h1>Strategic Machines</h1>
-			<h2>Secure Messaging</h2>
-			<h3>JSON Web Token Demo</h3>
-		</div>
+		<div className="homeContainer text-center">
+					<h1><strong className="text-white">Welcome to</strong></h1>
+					<br></br>
+					<img src={logo} alt="website logo" width="80%" height="auto" />
+					<hr className="my-4" style={{ border: "3px solid red", margin: "15%" }} />
+					<br></br>
+					<h4><strong className="text-white">Please sign in below to continue...</strong></h4>
+					<br></br>
+					<br></br>
+						<button type="button" className="selBtn">
+						<Link to="/login">Log In</Link>
+							</button>
+							<br>
+							</br>
+							<br>
+							</br>
+						<button type="button" className="selBtn">
+						<Link to="/signup">Sign Up</Link>
+						</button>
+				</div>
 	)
 }
 
