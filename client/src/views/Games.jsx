dc19c884cdf30b5e71b8854c532886425bfb0ada
Brock T <brockthigpen@yahoo.com>
2020-03-29 22:35:29 -0400

    removed unused variable

--- client/src/views/Games.jsx
+++ client/src/views/Games.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { render } from "react-dom";
+import { render } from 'react-dom'
 // import vipImage from '../vip.png'
-import AppContainer from "../containers/BirdContainers/AppContainer";
-import "../css/BirdGameCss/main.css";
+import AppContainer from '../containers/BirdContainers/AppContainer'
+import '../css/BirdGameCss/main.css'
 
-const Games = (props) => {
-	return (
-		<div className='VIP'>
-			{render(<AppContainer />, document.getElementById("root"))};
-		</div>
-	)
+const Games = () => {
+  return (
+    <div className="VIP">
+      {render(<AppContainer />, document.getElementById('root'))};
+    </div>
+  )
 }
 
 export default Games
