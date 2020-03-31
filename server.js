ef1ca9683f4cbed34210a593bfc34fc2aaf4fe36
Brock T <brockthigpen@yahoo.com>
2020-03-30 21:35:52 -0400

    trying to fix heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-const path = require('path')
+
 const express = require('express')
 const logger = require('morgan')
 const mongoose = require('mongoose')
 const usersRoutes = require('./routes')
-const publicPath = path.join(__dirname, '..', 'public');
 
 const app = express()
 
@@ -16,7 +15,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
 	console.log(err || `Connected to MongoDB.`)
 })
 
-app.use(express.static(publicPath))
+app.use(express.static(`${__dirname}/client/build`))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -28,7 +27,7 @@ app.get('/api', (req, res) => {
 app.use('/api/users', usersRoutes)
 
 app.use('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'))
+	res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
 app.listen(PORT, (err) => {
