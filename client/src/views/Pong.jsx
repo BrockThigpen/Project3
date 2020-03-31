e1e5dd6345a23364512dd8ac93cbb7db689505cb
Tcauley91 <caleytyler@gmail.com>
2020-03-30 21:46:41 -0400

    adding pong page

---
+++ client/src/views/Pong.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import PongContainer from '../containers/PongContainer.js/PongContainer'
+const Pong= () => {
+	return (
+		
+			render(<PongContainer/>, document.getElementById('root'))
+		
+	)
+}
+
+export default Pong
