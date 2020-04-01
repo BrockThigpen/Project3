5ac940a44e97e5e47bfc070ba135169a75f1a60a
Brock T <brockthigpen@yahoo.com>
2020-03-31 23:18:37 -0400

    added axios call for finding all users // added httpClient.getCurrentUser

--- client/src/views/Leaderboard.jsx
+++ client/src/views/Leaderboard.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import httpClient from '../httpClient';
 
 const Leaderboard = () => {
+  // get current user
+  const user = httpClient.getCurrentUser();
+  console.log(user._id);
+  // get all users
+  axios
+    .get(window.location.origin + '/api/users')
+    .then(res => console.log(res.data));
+
   return (
     <div>
       <h1>hello world</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
