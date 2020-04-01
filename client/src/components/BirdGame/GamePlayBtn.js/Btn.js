f722aca5975a780233192b54b3d8c467b4a10a66
Tcauley91 <caleytyler@gmail.com>
2020-04-01 19:44:15 -0400

    syntax

---
+++ client/src/components/BirdGame/GamePlayBtn.js/Btn.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Btn(props) {
+    
+    return <button className="gameBtn2">Home</button>;
+}
+
+export default Btn;
--- client/src/components/BirdGame/GamePlayBtns.js/Btns.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Btns(props) {
-    return <div id="score">{props.score}</div>;
-}
-
-export default Btns;
