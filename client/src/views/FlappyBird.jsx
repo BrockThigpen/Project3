efb3384fac96d5d8104a7e2a1e68766591b93dfe
Josh Craig <jcraig451@yahoo.com>
2020-04-01 19:03:40 -0400

    continue ui build

--- client/src/views/FlappyBird.jsx
+++ client/src/views/FlappyBird.jsx
@@ -4,9 +4,9 @@ import "../css/BirdGameCss/main.css";
 import AppContainer from '../containers/BirdContainers/AppContainer';
 const FlappyBird = () => {
 	return (
-		
+		<div className="gameContainer text-center">
 			render(<AppContainer />, document.getElementById('root'))
-		
+			</div>
 	)
 }
 
