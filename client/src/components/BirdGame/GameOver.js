c2be22da65aad8bf803741a24420376747e43629
Josh Craig <jcraig451@yahoo.com>
2020-04-01 19:42:27 -0400

    styling ball game

--- client/src/components/BirdGame/GameOver.js
+++ client/src/components/BirdGame/GameOver.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { Link } from 'react-router-dom'
 import "../../css/BirdGameCss/gameOver.css";
 
 function GameOver(props) {
     return (
-        <div id="gameOver">
+        <div id="gameOver" className="text-center">
             <div>Game Over!</div>
-            <button onClick={() => document.location.reload()}>Restart</button>
+            <button className="gameBtn2" onClick={() => document.location.reload()}>Restart</button>
+            <Link to="/Games">
+                <button className="gameBtn2">Home</button>
+            </Link>
         </div>
     );
 }
