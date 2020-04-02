7f69a534920ff0f34fb6823d4ac26a0499ac04da
Brock T <brockthigpen@yahoo.com>
2020-04-01 22:44:02 -0400

    linting

--- client/src/views/Pong.jsx
+++ client/src/views/Pong.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import PongContainer from '../containers/PongContainer.js/PongContainer'
-const Pong= () => {
-	return (
-		
-			render(<PongContainer/>, document.getElementById('root'))
-		
-	)
-}
+import React from 'react';
+import { render } from 'react-dom';
+import PongContainer from '../containers/PongContainer.js/PongContainer';
+const Pong = () => {
+  return render(<PongContainer />, document.getElementById('root'));
+};
 
-export default Pong
+export default Pong;
