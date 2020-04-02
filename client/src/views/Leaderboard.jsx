f357b0bed79d4e2c7746e7863a139903c93c3abe
Brock T <brockthigpen@yahoo.com>
2020-04-01 22:43:40 -0400

    testing

--- client/src/views/Leaderboard.jsx
+++ client/src/views/Leaderboard.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-import httpClient from '../httpClient';
+// import axios from 'axios';
+// import httpClient from '../httpClient';
 
 const Leaderboard = () => {
   // get current user
-  const user = httpClient.getCurrentUser();
-  console.log(user._id);
+  // const user = httpClient.getCurrentUser();
+  // console.log(user._id);
   // get all users
-  axios
-    .get(window.location.origin + '/api/users')
-    .then(res => console.log(res.data));
+  // axios.patch(window.location.origin + '/api/users/' + user._id);
+  // .then(res => console.log(res.data));
 
   return (
     <div>
