e7688e312491a8093e6b25291838792f1b299227
patrick dorgu <pat.dorgu@gmail.com>
2020-04-03 23:10:54 -0400

    added leaderboard

---
+++ client/src/components/leaderboard/leaderboard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
+
+
+
+function Table({playerData}) {
+    return (
+
+      <ReactTable
+      
+      columns={[{
+        Header:"name",
+        accessor:"name"},
+
+        {Header:"Dodgeball",
+        accesor:"game1"},
+
+       {Header:"Pong",
+       accesor:"game2"
+    }
+      
+
+
+
+
+      ]}
+      data={playerData}
+      
+      
+      
+      />
+    )
+}
+
+export default Table;
