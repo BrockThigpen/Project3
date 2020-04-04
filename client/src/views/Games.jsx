48622d419f2427ae6fa7147f7245ba074f3d4fdd
Josh Craig <jcraig451@yahoo.com>
2020-04-04 02:53:19 -0400

    games page updated

--- client/src/views/Games.jsx
+++ client/src/views/Games.jsx
@@ -7,12 +7,12 @@ const Games = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6 d-flex justify-content-center">
+        <div className="col-md-4 d-flex justify-content-center">
           <Link to="/FlappyBird">
             <img src={ball} alt="ball_game" className="gameApps" />
           </Link>
         </div>
-        <div className="col-md-6 d-flex justify-content-center">
+        <div className="col-md-4 d-flex justify-content-center">
           <Link to="/Pong">
             <img src={pong} alt="ball_game" className="gameApps" />
           </Link>
