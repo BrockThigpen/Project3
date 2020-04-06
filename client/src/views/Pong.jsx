29de30f9b853e20aaa9e37777fc3d2a43361760e
Brock T <brockthigpen@yahoo.com>
2020-04-05 20:05:29 -0400

    linting and formating

--- client/src/views/Pong.jsx
+++ client/src/views/Pong.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
-import PongContainer from '../containers/PongContainer/PongContainer'
-import upBtn from '../components/Pong/upBtn'
-import downBtn from '../components/Pong/downBtn'
+import React from 'react';
+import PongContainer from '../containers/PongContainer/PongContainer';
+import upBtn from '../components/Pong/upBtn';
+import downBtn from '../components/Pong/downBtn';
 
-const Pong= () => {
-	return (
-		<div className="gameContainer text-center">
-		<PongContainer />
-		<upBtn />
-		<downBtn />
-		</div>
-			// render(<PongContainer/>, document.getElementById('root'))
-	);
-}
+const Pong = () => {
+  return (
+    <div className="gameContainer text-center">
+      <PongContainer />
+      <upBtn />
+      <downBtn />
+    </div>
+    // render(<PongContainer/>, document.getElementById('root'))
+  );
+};
 
 export default Pong;
