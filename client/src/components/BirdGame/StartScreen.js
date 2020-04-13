898f2753ed9e30f201ea38f6ba44b62348eaa6bd
Tcauley91 <caleytyler@gmail.com>
2020-04-12 20:19:01 -0400

    syntax

--- client/src/components/BirdGame/StartScreen.js
+++ client/src/components/BirdGame/StartScreen.js
@@ -6,11 +6,10 @@ function StartScreen(props) {
     return (
         <div className="text-center" id="startScreen">
             <div className="flappyStart">
-                <p><strong>Press space bar to start the game...</strong></p></div>
+                <p><strong>Press space bar  OR tap screen to start the game...</strong></p></div>
             <Link to="/Games">
                 <button className="gameBtn2">Go Back</button>
             </Link>
-            <button className="gameBtn2" id='startGame' onMouseDown={props.isGameStarted}>Start Game</button>
         </div>
     );
 }
