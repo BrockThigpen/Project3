458efcb0720d62114e710443e0fb9d0c10bcfc80
Brock T <brockthigpen@yahoo.com>
2020-04-14 20:51:11 -0400

    removed unused buttons

--- client/src/components/BirdGame/App.js
+++ client/src/components/BirdGame/App.js
@@ -4,8 +4,6 @@ import PillarContainer from "../../containers/BirdContainers/PillarContainer";
 import Score from "./Score";
 import GameOver from "./GameOver";
 import StartScreen from "./StartScreen";
-// import FlyBtn from './FlyBtn'
-
 
 
 function App(props) {
@@ -13,19 +11,24 @@ function App(props) {
 
     return (
       <div>
-        {/* <FlyBtn /> */}
         <BirdContainer isGameOver={props.isGameOver}
           handleGameOver={props.handleGameOver}
           handleScore={props.handleScore} />
         <PillarContainer isGameOver={props.isGameOver} />
         <Score score={props.score} />
-
         {props.isGameOver && <GameOver />}
       </div>
     );
+    
   } else {
-    return <StartScreen />;
+    return (
+      
+      <StartScreen />
+
+    )
   }
+
+  
 }
 
 export default App;
--- client/src/components/BirdGame/StartScreen.js
+++ client/src/components/BirdGame/StartScreen.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import "../../css/BirdGameCss/startScreen.css";
-// import FlyBtn from './FlyBtn'
 
 function StartScreen(props) {
     return (
         <div className="text-center" id="startScreen">
-             {/* <FlyBtn/> */}
             <div className="flappyStart">
-                <p><strong>Press space bar to start the game...</strong></p></div>
+                <p><strong>Press space bar  OR tap screen to start the game...</strong></p></div>
             <Link to="/Games">
-                <button className="gameBtn2">Go Back</button>
+                <button id='backBtn' className="gameBtn2">Go Back</button>
             </Link>
         </div>
     );
